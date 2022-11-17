38d108d8b27679d6d5d2269637046b2fad4e758f
erdemir123 <sadikerdemir93@gmail.com>
2022-11-17 18:08:34 +0300

    midleware added

--- src/features/PostSlice.jsx
+++ src/features/PostSlice.jsx
@@ -1,10 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios  from 'axios';
+
 
 const initialState = {
 postList:[],
 loading:false,
 error:false
 }
+export const getPost = createAsyncThunk(
+    "getnews",
+    async(thunAPI,{rejectWithValue})=>{
+        const url =`https://jsonplaceholder.typicode.com/posts`
+        try {
+            const {data}= await axios(url)
+            return data
+        } catch (error) {
+            return rejectWithValue("something went value")
+        }
+    }
+)
 
 const PostSlice = createSlice({
   name: "post",
@@ -13,6 +27,19 @@ const PostSlice = createSlice({
     clearPost:(state,action)=>{
         state.postList=[]
     }
+  },
+  extraReducers:(builder)=>{
+    builder.addCase(getPost.pending,(state,action)=>{
+        state.loading=true
+    })
+    .addCase(getPost.fulfilled,(state,action)=>{
+        state.loading=false;
+        state.postList=action.payload
+    })
+    .addCase(getPost.rejected,(state,action)=>{
+        state.loading=false;
+        state.error=action.payload
+    })
   }
 });
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { clearUser } from '../features/AuthSlice'
+import { getPost } from '../features/PostSlice'
 
 const Home = () => {
     const dispatch = useDispatch()
   const {user}=useSelector((state)=>state.auth)
+  const {postList}=useSelector((state)=>state.post)
+  
+  useEffect(()=>{
+    dispatch(getPost())
+    
+  },[])
+  console.log(postList);
   return (
     <div>
       Hoşgeldiniz {user.name} {user.email} {user.password}
